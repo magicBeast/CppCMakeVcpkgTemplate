5fd8bf47dff6fc681c3386f7c99137e215ca4435
don <doninthailand@me.com>
2025-10-16 17:14:03 +0100

    Small tweak to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "imgui_impl_sdl3.h"
-#include "imgui_impl_sdlrenderer3.h"
+#include "backends/imgui_impl_sdl3.h"
+#include "backends/imgui_impl_sdlrenderer3.h"
 
 #include <SDL3/SDL.h>
 #include <imgui.h>
